9b5b37af641c87e44f45e98f6e2242ea55d0b148
negin-mgdm <negin.mgdm@gmail.com>
2024-12-22 01:14:34 +0000

    feat: Adding expense entries to the 'expenses' table.

--- main.py
+++ main.py
@@ -4,8 +4,24 @@ sqliteConnection = sqlite3.connect('sql.db')
 cursor = sqliteConnection.cursor()
 
 query = '''CREATE TABLE IF NOT EXISTS Expenses (
-    expense_id INTEGER PRIMARY KEY,
+    expense_id INTEGER PRIMARY KEY AUTOINCREMENT,
     category TEXT NOT NULL
     );'''
 
 cursor.execute(query)
+
+
+def add_expense_category():
+    query = ''' INSERT INTO Expenses (category)
+    VALUES 
+    ('utility'),
+    ('rent');
+    '''
+    cursor.execute(query)
+    sqliteConnection.commit()
+
+
+add_expense_category()
+
+cursor.close()
+sqliteConnection.close()
